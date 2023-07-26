05a2f73f11325c3b09ad7f902ef7f3bed8073d90
rishavdhungel <rishavdhungel3@gmail.com>
2023-07-26 19:20:39 +0545

    grid logic implemented.

---
+++ main.js
@@ -0,0 +1,43 @@
+const griditems = document.querySelector(".griditems");
+const controller = document.querySelectorAll(".gridbtn");
+console.log(controller);
+
+//creating a function that will create a div element with .div-cell class
+function createGridCell(){
+    let cell =document.createElement('div') ;
+    cell.classList.add('grid-cell');
+    griditems.appendChild(cell);
+}
+
+function changeStyle(gridcells){
+    if(gridcells == 16){
+        griditems.style.gridTemplateColumns = 'repeat(16, 1fr)';
+        griditems.style.gridTemplateRows = 'repeat(16, 1fr)';
+    }else if( gridcells == 50){
+        griditems.style.gridTemplateColumns = 'repeat(50, 1fr)';
+        griditems.style.gridTemplateRows = 'repeat(50, 1fr)';
+    }else if (gridcells == 100){
+        griditems.style.gridTemplateColumns = 'repeat(100, 1fr)';
+        griditems.style.gridTemplateRows = 'repeat(100, 1fr)';
+    }else{
+        alert("Happy Hacking!");
+    } 
+}
+
+//Creating a function to add a grid cells to the container
+function addGridCells(cellsnumber){
+    document.querySelectorAll('.grid-cell').forEach(e => e.remove());
+    changeStyle(cellsnumber);
+    let totalcells = cellsnumber**2;
+    for (let j = 0; j<totalcells; j++){
+        createGridCell();
+    }
+}
+// Adding Event Listener to every buttons
+for ( let i = 0; i <controller.length; i ++){
+    controller[i].addEventListener('click',function(){
+        let gridcells = controller[i].getAttribute('data-grid');
+        //we need to pass the gridcells to a function that appends the grid-cells to the gridcontainer
+        addGridCells(gridcells);
+    });
+}
